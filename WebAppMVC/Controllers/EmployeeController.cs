9d042aec77372fa4c8cd85b3b8d597b3c77569a3
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-12-01 02:35:27 +0300

    Get employee index page with jquery and datatable

---
+++ WebAppMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Employee> empList = db.Employees.ToList<Employee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
