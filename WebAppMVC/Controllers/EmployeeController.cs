436d3059fd9847b8ebeb24e2970d7d13b2206320
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-12-01 16:34:02 +0300

    Add ajax edit form and update the datatable records

--- WebAppMVC/Controllers/EmployeeController.cs
+++ WebAppMVC/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,7 +28,15 @@ namespace WebAppMVC.Controllers
         [HttpGet]
         public ActionResult AddOrEdit(int id = 0)
         {
-            return View(new Employee());
+            if (id == 0)
+                return View(new Employee());
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    return View(db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>());
+                }
+            }
         }
 
         [HttpPost]
@@ -35,9 +44,18 @@ namespace WebAppMVC.Controllers
         {
             using (DBModel db = new DBModel())
             {
-                db.Employees.Add(emp);
-                db.SaveChanges();
-                return Json(new { success=true, message="Save Successfuly"}, JsonRequestBehavior.AllowGet);
+                if (emp.EmployeeID == 0)
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success=true, message="Save Successfuly"}, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Update Successfuly" }, JsonRequestBehavior.AllowGet);
+                }
             }
         }
     }
